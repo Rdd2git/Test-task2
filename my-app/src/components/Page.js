704820b7a6eb494e976f34a8914455ad31855ad3
Radik GR <rgaliullin55@gmail.ru>
2020-03-19 18:04:16 +0500

    AddStep2_Page_File

---
+++ my-app/src/components/Page.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Page extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+        <p>
+          Cumque earum corrupti corporis similique quae culpa nesciunt maxime
+          odit, at doloremque itaque tempora, doloribus eum dolorum,
+          reprehenderit omnis sequi enim quam!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Page;
