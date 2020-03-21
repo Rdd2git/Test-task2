55e5a0cd77b252ff9cabbe9f4bfbe99ee37e0b1d
Radik GR <rgaliullin55@gmail.ru>
2020-03-21 14:36:52 +0500

    Commen_has_Added
    add comment structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Page from "./components/Page";
+import Comment from "./components/Comment";
 
 function App() {
   return (
     <div className="App">
       <Page />
+      <Comment />
     </div>
   );
 }
--- my-app/src/components/Comment.js
+++ my-app/src/components/Comment.js
@@ -1,1 +1,24 @@
+import React, { Component } from "react";
 
+class Comment extends Component {
+  render() {
+    return (
+      <div className="comment">
+        <div className="avatar">
+          <image href=""></image>
+        </div>
+        <div className="dateTime"></div>
+        <div className="avtor">Comment Avtor</div>
+        <div className="text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+          inventore perferendis quo debitis nobis a. Totam, tempore dolor cum
+          natus recusandae laudantium harum repudiandae labore molestias quam id
+          atque architecto!
+        </div>
+        <div className="reply"></div>
+      </div>
+    );
+  }
+}
+
+export default Comment;
